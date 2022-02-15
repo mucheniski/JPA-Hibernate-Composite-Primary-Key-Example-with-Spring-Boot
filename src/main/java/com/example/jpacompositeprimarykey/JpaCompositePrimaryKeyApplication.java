2fe029e6ac695c52968908193e868ea2cca8c7a1
Diego Mucheniski <mucheniski@gmail.com>
2022-02-15 11:19:01 -0300

    First commit

---
+++ src/main/java/com/example/jpacompositeprimarykey/JpaCompositePrimaryKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpacompositeprimarykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCompositePrimaryKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCompositePrimaryKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpacompositeprimarykey/JpaCompositePrimaryKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpacompositeprimarykey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCompositePrimaryKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
